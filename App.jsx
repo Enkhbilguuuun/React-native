1158858cfc8324d07d7ec21d6128607eb756bcb0
Батсайхан Энхбилгүүн <HP233073@M301-CC0140.local>
2023-06-20 14:00:49 +0800

    ASD

--- App.jsx
+++ App.jsx
@@ -1,19 +1,69 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, SafeAreaView, TouchableOpacity} from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+
+import { useEffect, useState } from "react";
 import Fun from "./components/Fun-component";
-import Todo from "./components/Todo"
+import Todo from "./components/Todo";
+import Quest from "./components/Quest";
 
 export default function App() {
+
+const [todo, setTodo] = useState([]);
+const [inputValue, setInputValue] = useState("")
+
+function map() {
+  return todo.map((e) => {
+    return <View>
+      <Quest value={e}/>
+      <TouchableOpacity onPress={}>
+          <FontAwesome5
+            name="trash"
+            size={20}
+            // color={clicked ? "white" : "black"}
+          />
+        </TouchableOpacity>
+    </View> 
+    
+  });
+}
+const add = () => {
+  console.log(inputValue)
+  setTodo([...todo, inputValue])
+}
+const checkarray = () => {
+  console.log(todo)
+}
+
+
   return (
-    <View style={styles.container}>
+  
+     <ScrollView style={styles.scrollview}>
+          <View style={styles.container}>
       <View style={styles.smallcont}>
-      <Text style={styles.header}>Todo Tasks</Text>
-      <Todo/>
-      <TextInput
-        style={styles.input}
-        placeholder="Write Tasks"/>
+        <Text style={styles.header}>Todo Tasks</Text>
+        {map()}
+        <TextInput
+         style={styles.input}
+         placeholder="Write Tasks"
+         onChangeText={(e) => setInputValue(e)} />
+        <Button
+          onPress={add}
+          style={styles.buttons}
+          title="Add"
+          color="#4d76ff"
+        />
+                <Button
+          onPress={checkarray}
+          style={styles.buttons}
+          title="checkarray"
+          color="#4d76ff"
+        />
       </View>
+      <StatusBar style="inverted"/>
     </View>
+     </ScrollView>
+ 
   );
 }
 
@@ -22,26 +72,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#dbdbdb",
     alignItems: "center",
-    justifyContent:"center"
+    justifyContent: "center",
   },
-  smallcont:{
-    backgroundColor:"purple",
-    height:400,
-    justifyContent:"space-evenly"
+  smallcont: {
+    height: "auto",
+    justifyContent: "space-evenly",
   },
-  input:{
+  input: {
     height: 40,
     width: 250,
     borderColor: "gray",
     borderWidth: 1,
     backgroundColor: "white",
-    alignSelf:"center",
-    borderRadius:6,
-    borderWidth:2,
-    borderColor:"gray"
+    alignSelf: "center",
+    borderRadius: 6,
+    borderWidth: 2,
+    borderColor: "gray",
   },
-  header:{
-    fontSize:24,
-    fontWeight:"bold"
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  scrollview:{
+    flex:1
   }
 });
+
+
+
+
--- components/Quest.jsx
+++ components/Quest.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import AiOutlineDelete from "react-icons/ai"
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useState } from "react";
 
 const Quest = ({ value }) => {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <View style={styles.cont}>
-        {/* <AiOutlineDelete/> */}
-      <Text style={styles.task}>{value}</Text>
+      {/* <BouncyCheckbox
+        size={25}
+        fillColor="red"
+        unfillColor="#FFFFFF"
+        text="Custom Checkbox"
+        iconStyle={{ borderColor: "red" }}
+        innerIconStyle={{ borderWidth: 2 }}
+        textStyle={{ fontFamily: "JosefinSans-Regular" }}
+        // onPress={(isChecked: boolean) => {}}
+      /> */}
+      <Text
+        style={clicked ? styles.whenclicked : styles.task}
+        onPress={() => {
+          setClicked(true);
+        }}
+      >
+        {value}{" "}
+        
+      </Text>
     </View>
   );
 };
@@ -21,7 +43,15 @@ const styles = StyleSheet.create({
     textAlignVertical: "center",
     textAlign: "center",
   },
-
+  whenclicked: {
+    backgroundColor: "gray",
+    color: "white",
+    height: 40,
+    width: 300,
+    textAlignVertical: "center",
+    textAlign: "center",
+    borderRadius: 5,
+  },
   cont: {
     margin: 5,
   },
